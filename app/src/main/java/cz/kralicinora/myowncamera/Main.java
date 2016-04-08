38daa8ed91d77cd7e86e4355ba373fc9051a127d
kralicinora <kralicinora@gmail.com>
2016-04-08 09:58:30 +0200

    Custom photo library

--- app/src/main/java/cz/kralicinora/myowncamera/Main.java
+++ app/src/main/java/cz/kralicinora/myowncamera/Main.java
@@ -3,20 +3,14 @@ package cz.kralicinora.myowncamera;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.commonsware.cwac.cam2.CameraActivity;
-import com.commonsware.cwac.cam2.Facing;
-import com.commonsware.cwac.cam2.ZoomStyle;
-
-import java.io.File;
-import java.util.Date;
-
 public class Main extends Activity {
-    TextView textView;
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,24 +21,27 @@ public class Main extends Activity {
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new CameraActivity.IntentBuilder(Main.this)
-                        .facing(Facing.BACK)
-                        .to(new File(Environment.getExternalStorageDirectory(), new Date().getTime() + ".jpg"))
-                        .zoomStyle(ZoomStyle.SEEKBAR)
-                        .skipConfirm()
-                        .allowSwitchFlashMode()
-                        .build();
-
-                startActivityForResult(i, 89);
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 42);
             }
         });
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
+        if (resultCode == RESULT_OK && requestCode == 42) {
             textView.setText(String.valueOf(data));
+            log("change3");
         }
+    }
+
+    private void log(String s) {
+        System.out.println("#LOG#> " + s);
     }
 }
